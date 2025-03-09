f26dcaa969287f38480ee7d4a455fe61309c2b2a
ROHITH UPPUNUTHULA <144814789+Rohith-dev-10@users.noreply.github.com>
2025-03-09 07:51:03 +0000

    2nd video

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1445533
+let accountEmail = "Rohith@gmail.com"
+var accountPassword  = "12345"
+ accounCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "Rohith"
+accounCity = "Warangal"
+console.table([accountEmail, accountId, accountPassword, accounCity])
+
+console.log(accountId);
