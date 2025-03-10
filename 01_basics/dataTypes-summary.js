15e72e3278d3d5c543ac584f340cfcd3f245c22b
ROHITH UPPUNUTHULA <144814789+Rohith-dev-10@users.noreply.github.com>
2025-03-10 14:05:42 +0000

    day 2

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,42 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1234567890n
+
+
+
+// reference (Non primitive)
+
+//Array, Objects, Functions
+
+
+const heros = ["rohith", "rakesh", "uppunuthula"]
+
+let myObj = {
+    name: "rohith",
+    age: 22
+}
+
+const myfunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(myfunction);
+
+
+console.log(typeof bigNumber);
